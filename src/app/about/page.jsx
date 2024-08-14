44228c906f2e343f5e883be72779add2aaab9b04
araf7053 <arafhusain0103@gmail.com>
2024-08-14 17:35:56 +0530

    updated home and about pages layout

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div>
+      <div className='text-4xl text-purple-800 text-center mt-2 p-2'> About</div>
+      <div className='flex gap-6 justify-around mt-24'>
+        <div className='w-2/5 h-60'> 
+          <img src="https://kinsta.com/wp-content/uploads/2021/11/about-us-page-1024x512.png" alt="image" />
+        </div>
+      <div className=' w-1/3'> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis quos, non ut possimus quibusdam doloribus molestiae, odit magni atque voluptas esse, numquam distinctio accusamus eum dolore rem reiciendis tempore iste. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ipsa aliquam repellat deserunt? Saepe rerum ab blanditiis eveniet nisi, sapiente voluptates quas tempore? Odio excepturi itaque molestias omnis corrupti totam dolor odit adipisci commodi explicabo! Consectetur similique obcaecati vero dolorem aspernatur alias odio sapiente quos inventore unde, sit laboriosam harum iste, velit, mollitia officiis tempora fugit? Temporibus, distinctio quisquam quaerat ipsa libero exercitationem dicta placeat repudiandae asperiores dolor quod corporis neque odio eius id impedit saepe nemo voluptatum blanditiis aut? </div></div>
+
+    </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className='text-red-700'> Home</div>
+    <div> 
+       <div className='text-6xl text-purple-800 text-center mt-20'> WELCOME TO NEXT AUTHENTICATION APP</div>
+       <p className='text-xl text-purple-500 text-center p-8'>Fast, Secure and the most reliable authentication app</p>
+       <img className='w-1/2 h-auto mx-96 ' src="https://www.loginradius.com/blog/static/6d170d83a7074847494831a6df861d3c/d3746/cover-auth.jpg" alt="image" />
+    </div>
   )
 }
